85b4e5f3dd21f1724f50f7e534b826310fb0b314
Farrah <marzini.f@gmail.com>
2022-10-07 09:55:40 +0200

    ajout pseudo code pour dropdown filter

--- scripts/app.js
+++ scripts/app.js
@@ -27,20 +27,20 @@ class App {
         });
     }
 
-    async createFilters() {
-        const Filtres = new Filters(this.recipes);
+    async createFilters(recipes) {
+        const Filtres = new Filters(recipes);
 
         this.ingredients = Filtres.allIngredients; 
         let $allIngredients = new FiltersView(this.ingredients, "Ingrédients");
-        this.$filterSection.querySelector('#ingredients').innerHTML += $allIngredients.render();
+        this.$filterSection.querySelector('#ingredients').innerHTML = $allIngredients.render();
 
         this.appliances = Filtres.allAppliances;
         let $allAppliances = new FiltersView(this.appliances , "Appareils");
-        this.$filterSection.querySelector('#appliances').innerHTML += $allAppliances.render();
+        this.$filterSection.querySelector('#appliances').innerHTML = $allAppliances.render();
     
         this.ustensils = Filtres.allUstensils;    
         let $allUstensils = new FiltersView(this.ustensils, "Ustensiles");
-        this.$filterSection.querySelector('#ustensils').innerHTML += $allUstensils.render();
+        this.$filterSection.querySelector('#ustensils').innerHTML = $allUstensils.render();
 
 
         this.updateFilter(Filtres);
@@ -60,6 +60,7 @@ class App {
         this.$filterSection.querySelectorAll('.dropdown__item').forEach( $item => {
             $item.addEventListener('click', e => {
                 let selectedFilter = e.target.textContent;
+                let selectedCat = e.target.getAttribute('data-belong');
 
                 this.recipes.forEach(recipe => {
                     
@@ -69,34 +70,57 @@ class App {
                 })
 
                 this.displayRecipes(this.filteredRecipes);
+                this.createFilters(this.filteredRecipes);
+                this.createTag(selectedFilter, selectedCat);
             });
         });
 
 
      // pseudo code 
      // OK !!! on selectionne un filtre (ici ustensil ) avec l'écoute d'evenement 
-     // OK !!! on prend sa valeur et la categorie de filtre à qui il appartient (ustensil)
-     // OK !!! on va faire passer Le filtre retenu (ici l'ustensil) dans un programme qui devra : 
-     // OK !!! - passer chaque recette et voir si le filtre (ustensil) est utilisé
+     // OK !!! on prend sa valeur et la categorie de filtre à qui elle appartient (ustensil)
+     // OK !!! on fait passer le filtre retenu (ici l'ustensil) dans un programme qui devra : 
+     // OK !!! Passer chaque recette et voir si le filtre (ustensil) est utilisé
      // OK !!! Si oui on stock la recette 
      // OK !!! A la fin de cette boucle, on recharge toutes les recettes stockées
-     // On ajoute l'étiquette 
-     // On remet à jour la liste des ustensils possible avec la nouvelle collection de recette
+     // OK !!! On ajoute l'étiquette 
+    //  Ajouter lecoute devenement sur les étiquettes pour suppression au clic 
+     // On remet à jour la liste des filtres (ustensils) possible avec la nouvelle collection de recette
      // On relance l'écoute devenement 
         
 
 
+    //  Autre pseudo code
+    // On selectionne un filtre avec l'écoute devenement
+    // On prend sa valeur et la categorie de filtre à laquelle elle appartient 
+    // on crée une étiquette associée et on lajoute à la liste de tag 
+    // on prend la liste de tag et on la fait passer dans un programme qui 
+    // - fait une boucle sur la liste 
+    // - prend chaque element (key = type de filtre & value = valeur)
+    // - je prends le tableau avec toutes les recettes (ou le nouveau tableau de recettes "recipesSelected" selectionnés sil existe ce qui signifie qu'il y a déjà un premier filtre qui est passé) 
+    // - à chaque itération je regarde si lelement (le filtre) en question est dans la recette 
+    // - si oui je stock la recette dans un nouveau tableau d'array "recipesSelected"
+    // a la fin de la boucle sur la liste de tag je :
+    // relance les filtres disponibles à partir de ma liste de recipesSelected
+    // Je relance les écoute d'évenement sur ces elements (tag + filtres)
+    
+    // nb: ajouter la fonctionnalité de retrait d'un tag avec bouton close
+
+    }
+
+    createTag(tag, cat) {
+
+        let $tag = `<span data-filterCat="${cat}" class="tag">${tag} <i class="fa-regular fa-circle-xmark"></i></span>`
+        document.querySelector('.tags').innerHTML += $tag;
     }
 
 
-
-    
 
     async start () {
         const datas = await this.api.get();
         this.recipes = datas.recipes; 
         this.displayRecipes(this.recipes);
-        this.createFilters();
+        this.createFilters(this.recipes);
     }
   }
   
--- scripts/views/Filter.js
+++ scripts/views/Filter.js
@@ -29,33 +29,6 @@ export default class Filter  {
 
 
 
-      
-
-    //     <div class="dropdown">
-    //     <button type="button" class="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false" data-bs-auto-close="outside">
-    //       Dropdown form
-    //     </button>
-    //     <form class="dropdown-menu p-4">
-    //       <div class="">
-    //         <label for="exampleDropdownFormEmail2" class="form-label">Email address</label>
-    //         <input type="email" class="form-control" id="exampleDropdownFormEmail2" placeholder="email@example.com">
-    //       </div>
-       
-       
-    //     </form>
-    //   </div>
-
-
-    //   <p class="filter__title" >Trier par</p>
-    //         <div class="filter__choice" id="filter__choice">
-    //             <button role="none" aria-haspopup="true" aria-expanded="false" aria-controls="allOptions" aria-labelledby="allOptions" aria-label="Afficher les options de tri" class="filter__button" id="tri"><span>Popularité</span><i class="fa-solid fa-angle-down"></i></button>
-    //             <ul id="allIngrediens" role="listbox" class="filter__options">
-    //              ${options}
-    //             </ul>
-            
-    //         </div>
-
-
         return `<div class="dropdown">
         <button type="button"  class="btn btn-lg btn-primary btn-${typeOfFilter} dropdown-toggle" data-filter="${typeOfFilter}"  data-bs-auto-close="outside">
           ${this.type}<i data-filter="${typeOfFilter}" class="fa-solid fa-chevron-down"></i>
